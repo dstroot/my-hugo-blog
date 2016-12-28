dec764ccef80542d227bdd85250d4d791287f67a
Dan Stroot <dan.stroot@gmail.com>
2016-12-28 07:23:15 -0800

    fixed images

--- gulpfile.js
+++ gulpfile.js
@@ -133,8 +133,9 @@ gulp.task('scripts', ['compile'], function () {
  */
 
 gulp.task('images', function () {
+  emitter.setMaxListeners()
   return gulp.src('static/img/*.{png,jpg,gif}')
-    .pipe($.changed('./static/img'))        // Only process new/changed
+    // .pipe($.changed('./static/img'))        // Only process new/changed
     .pipe($.imagemin({                      // Compress images
       progressive: true,   // JPG
       interlaced: true,    // GIF
