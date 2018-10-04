bd9049c5e1729cfabf31cdd806f7b5b658201b71
Dan Stroot <dan.stroot@gmail.com>
2018-10-04 06:31:40 -0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 /* jshint node: true */
 /* eslint-env node */
 
-'use strict';
-require('events').EventEmitter.prototype._maxListeners = 100;
+"use strict";
+require("events").EventEmitter.prototype._maxListeners = 100;
 
 // Install: you must install gulp both globally *and* locally.
 // Make sure you `$ npm install -g gulp`
@@ -19,26 +19,27 @@ require('events').EventEmitter.prototype._maxListeners = 100;
  * Dependencies
  */
 
-var gulp          = require('gulp');
-var $             = require('gulp-load-plugins')({ lazy: true });
-var runSequence   = require('run-sequence');
-var terminus      = require('terminus');
-var exec          = require('child_process').exec;
+var gulp = require("gulp");
+var $ = require("gulp-load-plugins")({ lazy: true });
+// var runSequence = require("run-sequence");
+var terminus = require("terminus");
+var exec = require("child_process").exec;
+var log = require("fancy-log");
 
 /**
  * Banner
  */
 
-var pkg = require('./package.json');
+var pkg = require("./package.json");
 var banner = [
-  '/**',
-  ' * <%= pkg.name %> - <%= pkg.description %>',
-  ' * @version v<%= pkg.version %>',
-  ' * @link <%= pkg.homepage %>',
-  ' * @license <%= pkg.license %>',
-  ' */',
-  ''
-].join('\n');
+  "/**",
+  " * <%= pkg.name %> - <%= pkg.description %>",
+  " * @version v<%= pkg.version %>",
+  " * @link <%= pkg.homepage %>",
+  " * @license <%= pkg.license %>",
+  " */",
+  ""
+].join("\n");
 
 /**
  * Paths
@@ -46,138 +47,134 @@ var banner = [
 
 var paths = {
   clean: [
-    'static/js/myblog*.js',
-    'static/css/*.css',
-    'static/css/*.map',
-    'docs/**',
-    '!docs',
-    '!docs/CNAME'
+    "static/js/myblog*.js",
+    "static/css/*.css",
+    "static/css/*.map",
+    "docs/**",
+    "!docs",
+    "!docs/CNAME"
   ],
   js: [],
-  lint: [
-    'gulpfile.js'
-  ],
+  lint: ["gulpfile.js"],
   html: [
-    'docs/**/*.html',
-    '!docs/lib/**/*.html'   // ignore
+    "docs/**/*.html",
+    "!docs/lib/**/*.html" // ignore
   ],
-  scss: [
-    'scss/**/*.scss'
-  ]
+  scss: ["scss/**/*.scss"]
 };
 
 /**
  * Clean
  */
 
-gulp.task('clean', function () {
-  var del           = require('del');
-  del(paths.clean);
+gulp.task("clean", function() {
+  var del = require("del");
+  return del(paths.clean);
 });
-
 
 /**
  * Process CSS
  */
 
-gulp.task('styles', function () {
-  var postcss      = require('gulp-postcss');
-  var sourcemaps   = require('gulp-sourcemaps');
-  var autoprefixer = require('autoprefixer');
-  var cssnano      = require('cssnano');
-  var sass         = require('gulp-sass');
+gulp.task("styles", function() {
+  var postcss = require("gulp-postcss");
+  var sourcemaps = require("gulp-sourcemaps");
+  var autoprefixer = require("autoprefixer");
+  var cssnano = require("cssnano");
+  var sass = require("gulp-sass");
 
   var processors = [
-    autoprefixer({ browsers: ['last 2 versions'] }),
+    autoprefixer({ browsers: ["last 2 versions"] }),
     cssnano({ safe: true })
   ];
 
-  return gulp.src('scss/styles.scss')
-    // .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(gulp.dest('./static/css'))        // Save CSS here
-    .pipe($.rename({ suffix: '.min' }))     // Add .min suffix
-    .pipe(postcss(processors))
-    // .pipe($.header(banner, { pkg : pkg }))  // Add banner
-    .pipe($.size({ title: 'CSS:' }))        // What size are we at?
-    // .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('static/css/'));
+  return (
+    gulp
+      .src("scss/styles.scss")
+      // .pipe(sourcemaps.init())
+      .pipe(sass())
+      .pipe(gulp.dest("./static/css")) // Save CSS here
+      .pipe($.rename({ suffix: ".min" })) // Add .min suffix
+      .pipe(postcss(processors))
+      // .pipe($.header(banner, { pkg : pkg }))  // Add banner
+      .pipe($.size({ title: "CSS:" })) // What size are we at?
+      // .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest("static/css/"))
+  );
 });
 
 /**
  * Build Highlight.js with the language packs needed.
  */
 
-gulp.task('compile', function (cb) {
-  return exec('cd static/lib/highlight && node tools/build.js -t browser bash css dockerfile go json scss xml yaml', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb();  // finished task
-  });
+gulp.task("compile", function(cb) {
+  return exec(
+    "cd static/lib/highlight && node tools/build.js -t browser bash css dockerfile go json scss xml yaml",
+    function(err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(); // finished task
+    }
+  );
 });
 
 /**
  * Move results of Highlight.js build
  */
 
-gulp.task('scripts', ['compile'], function () {
-  gulp.src('static/lib/highlight/build/highlight.pack.js')
-    .pipe(gulp.dest('./static/js'));
-  return gulp.src('static/lib/highlight/src/styles/*.css')
-    .pipe(gulp.dest('./static/css/highlight'));
+gulp.task("scripts", function() {
+  gulp
+    .src("static/lib/highlight/build/highlight.pack.js")
+    .pipe(gulp.dest("./static/js"));
+  return gulp
+    .src("static/lib/highlight/src/styles/*.css")
+    .pipe(gulp.dest("./static/css/highlight"));
 });
 
 /**
  * Process Images
  */
 
-gulp.task('images', function () {
-  var pngcrush      = require('imagemin-pngcrush');
-  return gulp.src('static/img/*.{png,jpg,gif}')
-    // .pipe($.changed('./static/img'))        // Only process new/changed
-    .pipe($.imagemin({                      // Compress images
-      progressive: true,   // JPG
-      interlaced: true,    // GIF
-      svgoPlugins: [{ removeViewBox: false }],  // SVG
-      use: [pngcrush()]    // PNG
-    }))
-    .on('error', console.error)
-    .pipe(gulp.dest('./static/img'));      // Write processed images
+gulp.task("images", function() {
+  var pngcrush = require("imagemin-pngcrush");
+  return (
+    gulp
+      .src("static/img/*.{png,jpg,gif}")
+      // .pipe($.changed('./static/img'))        // Only process new/changed
+      .pipe(
+        $.imagemin({
+          // Compress images
+          progressive: true, // JPG
+          interlaced: true, // GIF
+          svgoPlugins: [{ removeViewBox: false }], // SVG
+          use: [pngcrush()] // PNG
+        })
+      )
+      .on("error", console.error)
+      .pipe(gulp.dest("./static/img"))
+  ); // Write processed images
 });
 
 /**
  * JSHint Files
  */
 
-gulp.task('lint', function () {
-  return gulp.src(paths.lint)               // Read .js files
-    .pipe($.jshint())                       // lint .js files
-    .pipe($.jshint.reporter('jshint-stylish'));
-});
-
-/**
- * JSCS Files
- */
-
-gulp.task('jscs', function () {
-  return gulp.src(paths.lint)               // Read .js files
-    .pipe($.jscs())                         // jscs .js files
-    .on('error', function (e) {
-      $.util.log(e.message);
-      $.jscs().end();
-    })
-    .pipe(terminus.devnull({ objectMode: true }));
+gulp.task("lint", function() {
+  return gulp
+    .src(paths.lint) // Read .js files
+    .pipe($.jshint()) // lint .js files
+    .pipe($.jshint.reporter("jshint-stylish"));
 });
 
 /**
  * Hugo
  */
 
-gulp.task('hugo', function (cb) {
-  return exec('hugo', function (err, stdout, stderr) {
+gulp.task("hugo", function(cb) {
+  return exec("hugo", function(err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
-    cb();  // finished task
+    cb(); // finished task
   });
 });
 
@@ -185,8 +182,9 @@ gulp.task('hugo', function (cb) {
  * Validate HTML
  */
 
-gulp.task('htmlhint', function () {
-  return gulp.src(paths.html)
+gulp.task("htmlhint", function() {
+  return gulp
+    .src(paths.html)
     .pipe($.htmlhint())
     .pipe($.htmlhint.reporter());
 });
@@ -195,19 +193,21 @@ gulp.task('htmlhint', function () {
  * HTML Minify
  */
 
-gulp.task('htmlminify', function () {
-  return gulp.src(paths.html)
-    .pipe($.htmlmin({
-      collapseWhitespace: true,
-      collapseBooleanAttributes: true,
-      removeCommentsFromCDATA: true,
-      removeOptionalTags: true,
-      removeComments: true,
-      minifyJS: true
-    }))
-    .pipe(gulp.dest('./docs'));
+gulp.task("htmlminify", function() {
+  return gulp
+    .src(paths.html)
+    .pipe(
+      $.htmlmin({
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeCommentsFromCDATA: true,
+        removeOptionalTags: true,
+        removeComments: true,
+        minifyJS: true
+      })
+    )
+    .pipe(gulp.dest("./docs"));
 });
-
 
 /**
  * Service Worker Setup
@@ -230,11 +230,10 @@ gulp.task('htmlminify', function () {
  * we will also use cloudflare so that we can have ssl.
  */
 
-gulp.task('generate-service-worker', function (callback) {
-  var path = require('path');
-  var swPrecache = require('sw-precache');
-  var rootDir = 'docs';
-
+gulp.task("generate-service-worker", function(callback) {
+  var path = require("path");
+  var swPrecache = require("sw-precache");
+  var rootDir = "docs";
 
   var config = {
     cacheId: pkg.name,
@@ -243,32 +242,34 @@ gulp.task('generate-service-worker', function (callback) {
     // This allows you to test precaching behavior without worry about the cache preventing your
     // local changes from being picked up during the development cycle.
     handleFetch: true,
-    logger: $.util.log,
-    runtimeCaching: [{
-      // See https://github.com/GoogleChrome/sw-toolbox#methods
-      urlPattern: /runtime-caching/,
-      handler: 'cacheFirst',
-      // See https://github.com/GoogleChrome/sw-toolbox#options
-      options: {
-        cache: {
-          maxEntries: 1,
-          name: 'runtime-cache'
+    logger: log,
+    runtimeCaching: [
+      {
+        // See https://github.com/GoogleChrome/sw-toolbox#methods
+        urlPattern: /runtime-caching/,
+        handler: "cacheFirst",
+        // See https://github.com/GoogleChrome/sw-toolbox#options
+        options: {
+          cache: {
+            maxEntries: 1,
+            name: "runtime-cache"
+          }
         }
       }
-    }],
+    ],
     staticFileGlobs: [
       // rootDir + '/css/**.css',
       // rootDir + '/**.html',
       // rootDir + '/images/**.*',
       // rootDir + '/js/**.js',
-      rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'
+      rootDir + "/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}"
     ],
-    stripPrefix: rootDir + '/',
+    stripPrefix: rootDir + "/",
     // verbose defaults to false, but for the purposes of this demo, log more.
     verbose: false
   };
 
-  swPrecache.write(path.join(rootDir, 'service-worker.js'), config, callback);
+  swPrecache.write(path.join(rootDir, "service-worker.js"), config, callback);
 
   // swPrecache.write(`${rootDir}/service-worker.js`), {
   //   staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
@@ -280,33 +281,50 @@ gulp.task('generate-service-worker', function (callback) {
  * Git it!
  */
 
-gulp.task('git', function (cb) {
-  return exec('git pull && git add -A && git commit -m "update" && git push', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb();  // finished task
-  });
+gulp.task("git", function(cb) {
+  return exec(
+    "git pull && git add -A && git commit -m 'update' && git push",
+    function(err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(); // finished task
+    }
+  );
 });
-
 
 /**
  * Default Task
  *   - Build all the things...
  */
 
-gulp.task('default', function (cb) {
-  runSequence(
-    'clean',
-    ['styles', 'images', 'lint', 'jscs'],  // , 'scripts'
-    'hugo',
-    'htmlminify',
-    cb);
-});
+// gulp.task("default", function(cb) {
+//   runSequence(
+//     "clean",
+//     ["styles", "images", "lint"], // , 'scripts'
+//     "hugo",
+//     "htmlminify",
+//     cb
+//   );
+// });
+
+gulp.task(
+  "default",
+  gulp.series(
+    "clean",
+    // "compile",
+    // "scripts",
+    gulp.parallel("styles", "images", "lint"),
+    "hugo",
+    "htmlminify",
+    "generate-service-worker",
+    "git"
+  )
+);
 
 /**
  * Watch Task
  */
 
-gulp.task('watch', function () {
-  gulp.watch(paths.scss, ['styles']);
+gulp.task("watch", function() {
+  gulp.watch(paths.scss, ["styles"]);
 });
